0c20fbd87f6d1f699a3f39aff57466b5d91f7b1c
dlipsa <dlipsa@d596a3c1-de11-4375-84b4-10211c464620>
2009-07-31 17:52:08 +0000

    git-svn-id: file:///home/danlipsa/dan-svn-repo/src@833 d596a3c1-de11-4375-84b4-10211c464620

---
+++ stable.h
@@ -0,0 +1,25 @@
+// Add C includes here
+#include <limits.h>
+#include <math.h>
+#include <string.h>
+
+#if defined __cplusplus
+// Add C++ includes here
+#include <algorithm>
+#include <exception>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <QApplication>
+#include <QGLWidget>
+#include <QtGui>
+#include <QtOpenGL>
+#include <QWidget>
+
+#include <G3D/Vector3.h>
+#endif
