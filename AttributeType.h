539363e526b28724887c2bf4bc93e474fecf8ff1
dlipsa <dlipsa@d596a3c1-de11-4375-84b4-10211c464620>
2009-08-17 04:17:24 +0000

    git-svn-id: file:///home/danlipsa/dan-svn-repo/src@853 d596a3c1-de11-4375-84b4-10211c464620

---
+++ AttributeType.h
@@ -0,0 +1,12 @@
+#ifndef __ATTRIBUTE_TYPE_H__
+#define __ATTRIBUTE_TYPE_H__
+
+enum AttributeType {
+    VERTEX_TYPE,
+    EDGE_TYPE,
+    FACE_TYPE,
+    BODY_TYPE,
+    ATTRIBUTE_TYPE_COUNT
+};
+
+#endif
