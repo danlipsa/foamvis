e90b8e58b2cb4e1ef3f01e0f0f8d22ba7d037fda
dlipsa <dlipsa@d596a3c1-de11-4375-84b4-10211c464620>
2010-02-02 17:30:16 +0000

    git-svn-id: file:///home/danlipsa/dan-svn-repo/src@1070 d596a3c1-de11-4375-84b4-10211c464620

---
+++ DataFiles.cpp
@@ -0,0 +1,46 @@
+/**
+ * @file DataFiles.cpp
+ * @author Dan R. Lipsa
+ *
+ * Method implementation for a list of Data objects
+ */
+
+#include "DataFiles.h"
+#include "ElementUtils.h"
+
+ostream& operator<< (ostream& ostr, DataFiles& dataFiles)
+{
+    ostr << "DataFiles: " << endl;
+    ostr << dataFiles.m_AABox << endl;
+    PrintElements<Data*> (ostr, dataFiles.m_data, "Data", true);
+    return ostr;
+}
+
+void DataFiles::Calculate (
+    MinMaxElement minMaxElement,
+    const G3D::Vector3& (Data::*corner) () const,
+    G3D::Vector3& v)
+{
+    using namespace G3D;
+    vector<Data*>::iterator it;
+    it = minMaxElement (m_data.begin (), m_data.end (), 
+	    Data::LessThan(Vector3::X_AXIS, corner));
+    v.x = ((*it)->*corner) ().x;
+
+    it = minMaxElement (m_data.begin (), m_data.end (), 
+	    Data::LessThan(Vector3::Y_AXIS, corner));    
+    v.y = ((*it)->*corner) ().y;
+
+    it = minMaxElement (m_data.begin (), m_data.end (), 
+	    Data::LessThan(Vector3::Z_AXIS, corner));
+    v.z = ((*it)->*corner) ().z;
+}
+
+void DataFiles::CalculateAABox ()
+{
+    using namespace G3D;
+    Vector3 low, high;
+    Calculate (min_element, &Data::GetAABoxLow, low);
+    Calculate (max_element, &Data::GetAABoxHigh, high);
+    m_AABox.set (low, high);
+}
---
+++ DataFiles.h
@@ -0,0 +1,73 @@
+/**
+ * @file DataFiles.h
+ * @author Dan R. Lipsa
+ *
+ * A list of Data objects
+ */
+#ifndef __DATA_FILES_H__
+#define __DATA_FILES_H__
+
+#include "Data.h"
+
+/**
+ * Stores information about a list of DMP files
+ */
+class DataFiles
+{
+public:
+    /**
+     * Iterator over a vector of Data
+     */
+    typedef vector<Data*>::iterator IteratorData;
+    /**
+     * Functor applied to a collection of Data objects
+     */
+    typedef IteratorData (*MinMaxElement)(IteratorData first,
+					  IteratorData last,
+					  Data::LessThan lessThan);
+    /**
+     * Calculate the  axially aligned bounding box for  this vector of
+     * Data objects
+     */
+    void CalculateAABox ();
+    /**
+     * Gets the vector of Data objects
+     * @return vector of data objects
+     */
+    vector<Data*>& GetData () {return m_data;}
+    /**
+     * Gets the AABox for this vector of Data objects
+     */
+    const G3D::AABox& GetAABox () {return m_AABox;}
+    /**
+     * Pretty print the DataFile object
+     */
+    friend ostream& operator<< (ostream& ostr, DataFiles& dataFiles);
+
+private:
+    /**
+     * Calculates the low/high point for the AABox of this list of Data objects
+     * @param minMaxElement functor to be applied to the list of data objects
+     * @param corner pointer to a function member of Data that returns
+     * the low or high corner of AABox of the data object
+     * @param v where to store the low/high point
+     */
+    void Calculate (
+	MinMaxElement minMaxElement,
+	const G3D::Vector3& (Data::*corner) () const,
+	G3D::Vector3& v);
+    /**
+     * Vector of Data objects
+     */
+    vector<Data*> m_data;
+    /**
+     * The AABox for this vector of Data objects
+     */
+    G3D::AABox m_AABox;
+};
+
+#endif //__DATA_FILES_H__
+
+// Local Variables:
+// mode: c++
+// End:
