cd5eca0fc98074cda16c1c308fcb2a7ac7fee5c0
dlipsa <dlipsa@d596a3c1-de11-4375-84b4-10211c464620>
2010-05-21 08:19:24 +0000

    git-svn-id: file:///home/danlipsa/dan-svn-repo/src@1308 d596a3c1-de11-4375-84b4-10211c464620

---
+++ OrientedFaceIndex.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file   Body.cpp
+ * @author Dan R. Lipsa
+ * @date 20 May 2010
+ *
+ * Implementation of the OrientedFaceIndex class
+ */
+
+#include "OrientedFaceIndex.h"
+#include "OrientedFace.h"
+
+ostream& operator<< (ostream& ostr, const OrientedFaceIndex& ofi)
+{
+    OrientedFace of (ofi.m_face, ofi.m_faceReversed);
+    ostr << "(oface=" << of.GetSignedId () << ", " 
+	 << "index=" << ofi.m_edgeIndex << ")";
+    return ostr;
+}
