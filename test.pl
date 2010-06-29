b5e8b162d98164c48d841769acb9bd51795ae69a
dlipsa <dlipsa@d596a3c1-de11-4375-84b4-10211c464620>
2010-06-29 14:42:03 +0000

    git-svn-id: file:///home/danlipsa/dan-svn-repo/src@1348 d596a3c1-de11-4375-84b4-10211c464620

--- test.pl
+++ test.pl
@@ -1,7 +1,28 @@
 #!/usr/bin/perl
-./foam ~/Documents/swansea-phd/foam/CTRCTN/dump_0.1520_0.2400_8.0000_0001.dmp
-./foam ~/Documents/swansea-phd/foam/PERIODICSHEAR/shear_65_0001_2.dmp
-./foam ~/Documents/swansea-phd/foam/COARSE3D/summary_30_0001_sph_free.dmp
-./foam ~/Documents/swansea-phd/foam/shear_160/shear_160_0000_1.dmp
-./foam ~/Documents/swansea-phd/foam/straight_5x2q_2per/straightq_2per_10_0001.dmp
-./foam ~/Documents/swansea-phd/foam/straight_6x2q_2/straightq_2_12_0001.dmp
+
+use strict;
+use warnings;
+
+my $location = "/home/dlipsa/Documents/phd-swansea/foam";
+my @tests = 
+    ("CTRCTN/dump_0.1520_0.2400_8.0000_0001.dmp",
+     "PERIODICSHEAR/shear_65_0001_2.dmp",
+     "COARSE3D/summary_30_0001_sph_free.dmp",
+     "shear_160/shear_160_0000_1.dmp",
+     "straight_5x2q_2per/straightq_2per_10_0001.dmp",
+     "straight_6x2q_2/straightq_2_12_0001.dmp");
+
+sub main ()
+{
+    foreach (@tests)
+    {
+	my $test = $_;
+	@args = ("./foam", $test);
+	system(@args) == 0
+	    or die "system @args failed: $?"
+    }
+}
+
+main ();
+
+
