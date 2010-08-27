8e1bfa187b8597c0316163c599769a4e4b9ea93b
dlipsa <dlipsa@d596a3c1-de11-4375-84b4-10211c464620>
2010-08-27 18:03:16 +0000

    @@
    git-svn-id: file:///home/danlipsa/dan-svn-repo/src@1451 d596a3c1-de11-4375-84b4-10211c464620

---
+++ Application.cpp
@@ -0,0 +1,41 @@
+/**
+ * @file   Application.cpp
+ * @author Dan R. Lipsa
+ * @date 27 August 2010
+ *
+ * Definition of the Application class
+ */
+
+#include "Application.h"
+#include "Debug.h"
+#include "DebugStream.h"
+
+boost::shared_ptr<Application> Application::m_application;
+
+
+boost::shared_ptr<Application> Application::Get (int& c, char** v)
+{
+    if (m_application == 0)
+	m_application.reset (new Application (c, v));
+    return m_application;
+}
+
+boost::shared_ptr<Application> Application::Get ()
+{
+    if (m_application == 0)
+	ThrowException ("The application object have to be created first");
+    return m_application;
+}
+
+bool Application::notify(QObject *rec, QEvent *ev)
+{
+    try
+    {
+	return QApplication::notify(rec, ev);
+    }
+    catch (const exception& e)
+    {
+	cdbg << "Exception: " << e.what () << endl;
+	return false;
+    }
+}
