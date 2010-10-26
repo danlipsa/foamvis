0ef856e3fc43552e1580bb42589b94d38840a9a3
dlipsa <dlipsa@d596a3c1-de11-4375-84b4-10211c464620>
2010-10-26 17:32:36 +0000

    git-svn-id: file:///home/danlipsa/dan-svn-repo/src@1559 d596a3c1-de11-4375-84b4-10211c464620

---
+++ DisplayElement.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file   DisplayElement.h
+ * @author Dan R. Lipsa
+ * @date  26 Oct 2010
+ *
+ * Implementation for functors that display an element 
+ * (body, face, edge or vertex)
+ */
+
+#include "DisplayElement.h"
+
+void TexCoordSetter::operator () (double value)
+{
+    double texCoord = m_glWidget.TexCoord (value);
+    glTexCoord1f (texCoord); 
+}
