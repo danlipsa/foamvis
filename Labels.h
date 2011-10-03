9e71463c3005c102bcebf7fc9a8f040c6f7a404f
dlipsa <dlipsa@d596a3c1-de11-4375-84b4-10211c464620>
2011-10-03 14:36:58 +0000

    git-svn-id: file:///home/danlipsa/dan-svn-repo/src@2170 d596a3c1-de11-4375-84b4-10211c464620

---
+++ Labels.h
@@ -0,0 +1,22 @@
+/**
+ * @file   Labels.h
+ * @author Dan R. Lipsa
+ * @date 3 Oct 2011
+ *
+ * Declaration of the Labels class
+ */
+
+#ifndef __LABELS_H__
+#define __LABELS_H__
+
+struct Labels
+{
+    vector<string> m_values;
+};
+
+
+#endif //__LABELS_H__
+
+// Local Variables:
+// mode: c++
+// End:
