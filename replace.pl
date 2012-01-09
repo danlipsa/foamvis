e24848ffd9baea7238295aa61ecdda0cfed12d10
dlipsa <dlipsa@d596a3c1-de11-4375-84b4-10211c464620>
2012-01-09 17:25:31 +0000

    git-svn-id: file:///home/danlipsa/dan-svn-repo/src@2304 d596a3c1-de11-4375-84b4-10211c464620

--- replace.pl
+++ replace.pl
@@ -3,20 +3,22 @@ use File::Copy;
 use strict;
 use warnings;
 
-
+# Replaces all occurences of strings in file `inName`. The strings to
+# be replaced and their substitutions are passed as a list of pairs:
+# (string, replacement)
 sub replace
 {
-    my ($name, $substitutions) = @_;
+    my ($inName, $pairs) = @_;
     my $outName = "temp.txt";
-    print "processing $name ...\n";
-    open (my $in, "<", $name)
-	or die ("Could not open $name: $!\n");
+    print "processing $inName ...\n";
+    open (my $in, "<", $inName)
+	or die ("Could not open $inName: $!\n");
     open (my $out, ">", $outName)
 	or die ("Could not open $outName: $!\n");
     while (<$in>)
     {
 	my $line = $_;
-	foreach (@{$substitutions})
+	foreach (@{$pairs})
 	{
 	    my ($src, $dest) = @{$_};
 	    $line =~ s/$src/$dest/;
@@ -27,6 +29,37 @@ sub replace
 	or die "$in: $!";
     close ($out)
 	or die "$out: $!";
-    move($outName, $name);
+    move($outName, $inName);
 }
 
+# Replaces all occurences of blocks in file `inName`. A block is marked with
+# BEGIN_COMMENT begin <file> END_COMMENT
+# ...
+# BEGIN_COMMENT end END_COMMENT
+# Blocks cannot be nested. All lines between the markers will be replaced with 
+# the contet of the <file>. Markers are left in place.
+sub replaceBlock
+{
+    my ($inName, $beginComment, $endComment) = @_;
+    my $outName = "temp.txt";
+    print "processing $inName ...\n";
+    open (my $in, "<", $inName)
+	or die ("Could not open $inName: $!\n");
+    open (my $out, ">", $outName)
+	or die ("Could not open $outName: $!\n");
+    while (<$in>)
+    {
+	my $line = $_;
+	foreach (@{$pairs})
+	{
+	    my ($src, $dest) = @{$_};
+	    $line =~ s/$src/$dest/;
+	}
+	print $out $line;
+    }
+    close ($in)
+	or die "$in: $!";
+    close ($out)
+	or die "$out: $!";
+    move($outName, $inName);
+}
