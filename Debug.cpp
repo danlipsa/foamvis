91d572c29d55d9ae6975f290b593d02b9159a374
dlipsa <dlipsa@d596a3c1-de11-4375-84b4-10211c464620>
2013-04-01 17:47:14 +0000

    git-svn-id: file:///home/danlipsa/dan-svn-repo/src@2783 d596a3c1-de11-4375-84b4-10211c464620

---
+++ Debug.cpp
@@ -0,0 +1,58 @@
+/**
+ * @file   Debug.h
+ * @author Dan R. Lipsa
+ *
+ * Stream for printing  debug messages. All debug messages  have to go
+ * to cdbg.  It works with both MS Visual C++ and with GCC
+ */
+
+#include "Debug.h"
+
+#ifdef _MSC_VER
+
+DebugStream cdbg;
+
+#else //_MSC_VER
+
+ostream& cdbg = cerr;
+
+#endif //_MSC_VER
+
+
+MeasureTime::MeasureTime ()
+{
+    StartInterval ();
+}
+
+void MeasureTime::StartInterval ()
+{
+    m_start = clock ();
+}
+
+void MeasureTime::EndInterval (const char* intervalName)
+{
+    clock_t end = clock ();
+    cdbg << intervalName << ": " 
+	 << (end - m_start) * 1000.0 / CLOCKS_PER_SEC << " ms" << endl;
+    m_start = end;
+}
+
+void MeasureTimeVtk::Execute (
+    vtkObject *caller, unsigned long eventId, void *callData)
+{
+    (void)callData;
+    if (eventId == vtkCommand::StartEvent)
+    {
+	m_measure.StartInterval ();
+    }
+    else if (eventId == vtkCommand::EndEvent)
+    {
+	m_measure.EndInterval (caller->GetClassName ());
+    }
+}
+
+void MeasureTimeVtk::Measure (vtkObject* caller)
+{
+    caller->AddObserver (vtkCommand::StartEvent, this);
+    caller->AddObserver (vtkCommand::EndEvent, this);
+}
