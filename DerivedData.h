29deebe6a79ed814d0fad13f95889a268498ad59
dlipsa <dlipsa@d596a3c1-de11-4375-84b4-10211c464620>
2013-07-04 20:25:55 +0000

    git-svn-id: file:///home/danlipsa/dan-svn-repo/src@2978 d596a3c1-de11-4375-84b4-10211c464620

---
+++ DerivedData.h
@@ -0,0 +1,34 @@
+/**
+ * @file   DerivedData.h
+ * @author Dan R. Lipsa
+ * @date 4 July 2011
+ * @ingroup model
+ * @brief Data derived from simulation data.
+ */
+#ifndef __DERIVED_DATA_H__
+#define __DERIVED_DATA_H__
+
+class AverageCacheT1KDEVelocity;
+class ObjectPositions;
+
+
+/**
+ * @brief Data derived from simulation data.
+ */
+struct DerivedData
+{
+    DerivedData (boost::shared_ptr<AverageCacheT1KDEVelocity> ac,
+                 boost::shared_ptr<ObjectPositions> op) :
+        m_averageCache (ac), m_objectPositions (op)
+    {
+    }
+    boost::shared_ptr<AverageCacheT1KDEVelocity> m_averageCache;
+    boost::shared_ptr<ObjectPositions> m_objectPositions;
+};
+
+
+#endif //__DERIVED_DATA_H__
+
+// Local Variables:
+// mode: c++
+// End:
